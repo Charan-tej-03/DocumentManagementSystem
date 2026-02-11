adfb345e3ac3bbc601571b99c9862e917566a52f
G Charan Tej Paul <charantejgudipallee@gmail.com>
2026-02-11 17:43:47 +0530

    testing mongodb connection

---
+++ backend/db_connect.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const connection = async() => {
+  try{
+    await mongoose.connect("mongodb://localhost:27017/project_db");
+    console.log("Connection sucessfull!!");
+  }catch(err){
+    console.error(err.message);
+  }
+};
+
+// module.exports = connection;
+
+connection();
